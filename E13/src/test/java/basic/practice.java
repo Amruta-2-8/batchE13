a9c533eaedcbdc184528bc460c01d48c10587d08
amruta <raskondaamruta32@gamil.com>
2025-07-29 16:37:17 +0530

    commit

---
+++ E13/src/test/java/basic/practice.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HElllo");
+		System.out.println("HElllo");
+		System.out.println("HElllo");
+		System.out.println("HElllo");
+		
+	}
+
+}
